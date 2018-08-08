6041e2d939050575355739fac49975776de364a3
gavrilyuc <gavrilyuc@gmail.com>
2018-08-08 11:56:33 +0300

    initial commit

---
+++ src/polygon.js
@@ -0,0 +1,331 @@
+$.fn.polygon = function(properties = {}) {
+    var currentElement = this;
+    var canvas = this.get(0);
+    var context = canvas.getContext('2d');
+    var globalProperties = $.extend({
+        boneSize: {
+            width: 10,
+            height: 10
+        },
+        colors: {
+            bone: {
+                selected: 'red',
+                default: 'rgba(255,0,0,0.6)'
+            },
+            polygon: {
+                selected: 'rgba(255,0,0,0.6)',
+                default: 'rgba(0,255,0,0.6)'
+            },
+            polygonFromType: {
+                1: {
+                    selected: 'rgba(255,0,0,0.6)',
+                    default: 'rgba(0,255,0,0.6)'
+                },
+                2: {
+                    selected: 'rgba(255,0,0,0.6)',
+                    default: 'rgba(0,255,0,0.6)'
+                },
+                3: {
+                    selected: 'rgba(255,0,0,0.6)',
+                    default: 'rgba(0,255,0,0.6)'
+                }
+            }
+        },
+        polygonSelectId: '#polygonType'
+    }, properties);
+    var polygons = [];
+
+    function invalidateAll()
+    {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        polygons.forEach(drawPolygon);
+    }
+    function drawPolygon(polygonItem)
+    {
+        context.save();
+        context.beginPath();
+        
+        polygonItem.points.forEach(function(p, i)
+        {
+            context.lineTo(p.x, p.y);
+        });
+        
+        var defaultPolygon = true;
+        for(var index in globalProperties.colors.polygonFromType)
+        {
+            if (polygonItem.polygonType == index)
+            {
+                var type = globalProperties.colors.polygonFromType[index];
+                context.fillStyle = polygonItem.isSelected ? type.selected : type.default;
+                defaultPolygon = false;
+                break;
+            }
+        }
+
+        if (defaultPolygon)
+        {
+            context.fillStyle = polygonItem.isSelected ? globalProperties.colors.polygon.selected : globalProperties.colors.polygon.default;
+        }
+        context.fill();
+        context.stroke();
+        context.closePath();
+        context.restore();
+
+        polygonItem.points.forEach(function(p, i)
+        {
+            context.save();
+            context.translate(p.x, p.y);
+            context.fillStyle = p.isSelected ? globalProperties.colors.bone.selected : globalProperties.colors.bone.default;
+            context.fillRect(-(globalProperties.boneSize.width / 2), -(globalProperties.boneSize.height / 2),
+                globalProperties.boneSize.width, globalProperties.boneSize.height);
+            context.restore();
+        });
+    }
+
+    function createNewPolygon(polygonType, x, y)
+    {
+        var p = {
+            points: [],
+            isSelected: false,
+            polygonType: polygonType
+        };
+        
+        var point = {
+            x: x,
+            y: y,
+            isSelected: false
+        };
+        p.points.push(point);
+
+        var point2 = {
+            x: x,
+            y: y,
+            isSelected: false
+        };
+        p.points.push(point2);
+
+        polygons.push(p);
+
+        invalidateAll();
+
+        return polygons.length - 1;
+    }
+    function addPointToPolygon(polygonIndex, x, y)
+    {
+        var point = {
+            x: x,
+            y: y,
+            isSelected: false
+        };
+        polygons[polygonIndex].points.splice(polygons[polygonIndex].points.length - 2, 0, point);
+
+        invalidateAll();
+    }
+
+    function hasPolygonTouch(x, y, polygonIndex)
+    {
+        var points = polygons[polygonIndex].points;
+        var i, j = points.length - 1, touch = false;
+
+        for (i = 0; i < points.length; i++)
+        {
+            var pxi = points[i].x, pxj = points[j].x,
+                pyi = points[i].y, pyj = points[j].y;
+
+            if((((pyi < y && pyj >= y) || (pyj < y && pyi >= y)) && (pxi <= x || pxj <= x))
+                &&
+                (pxi + (y - pyi) / (pyj - pyi) * (pxj - pxi) < x))
+            {
+                touch = !touch;
+            }
+            j = i;
+        }
+
+        return touch;
+    }
+    function getIndexBoneTouch(x, y, polygonIndex)
+    {
+        var points = polygons[polygonIndex].points;
+
+        // because first element = last
+        for(var i = 0; i < points.length - 1; i++)
+        {
+            // bone located is center point
+            var bone = {
+                left: points[i].x - (globalProperties.boneSize.width / 2),
+                top: points[i].y - (globalProperties.boneSize.height / 2),
+                right: points[i].x + globalProperties.boneSize.width,
+                bottom: points[i].y + globalProperties.boneSize.height
+            };
+            
+            if ((bone.left <= x && bone.right >= x) && (bone.top <= y && bone.bottom >= y))
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    function init()
+    {
+        var _indexTouchedPolygon = -1;
+        var _indexTouchedBone = -1;
+        var startX = 0, startY = 0;
+
+        $(currentElement).mousedown(function (event)
+        {
+            event.preventDefault();
+            var x = event.offsetX;
+            var y = event.offsetY;
+
+            if (event.which != 1)
+            {
+                return;
+            }
+
+            if (event.ctrlKey && _indexTouchedPolygon == -1)
+            {
+                _indexTouchedPolygon = createNewPolygon($(globalProperties.polygonSelectId).val(), x, y);
+                return;
+            }
+
+            if (event.ctrlKey && _indexTouchedPolygon > -1)
+            {
+                addPointToPolygon(_indexTouchedPolygon, x, y);
+                return;
+            }
+
+            for(var i = 0; i < polygons.length; i++)
+            {
+                var boneIndex = getIndexBoneTouch(x, y, i);
+                if (boneIndex > -1)
+                {
+                    startX = x;
+                    startY = y;
+                    _indexTouchedBone = boneIndex;
+                    _indexTouchedPolygon = i;
+                    polygons[i].points[boneIndex].isSelected = true;
+                    break;
+                }
+
+                if (hasPolygonTouch(x, y, i))
+                {
+                    _indexTouchedPolygon = i;
+                    startX = x;
+                    startY = y;
+                    polygons[i].isSelected = true;
+                    break;
+                }
+            }
+        });
+
+        $(currentElement).mousemove(function (event)
+        {
+            event.preventDefault();
+            var x = event.offsetX;
+            var y = event.offsetY;
+
+            if (_indexTouchedPolygon == -1)
+            {
+                return;
+            }
+
+            if (_indexTouchedBone > -1 && polygons[_indexTouchedPolygon].points[_indexTouchedBone].isSelected)
+            {
+                polygons[_indexTouchedPolygon].points[_indexTouchedBone].x = x;
+                polygons[_indexTouchedPolygon].points[_indexTouchedBone].y = y;
+
+                // if first bone need move first and last point
+                if (_indexTouchedBone == 0)
+                {
+                    polygons[_indexTouchedPolygon].points[polygons[_indexTouchedPolygon].points.length - 1].x = x;
+                    polygons[_indexTouchedPolygon].points[polygons[_indexTouchedPolygon].points.length - 1].y = y;
+                }
+
+                invalidateAll();
+                return;
+            }
+
+            if (!polygons[_indexTouchedPolygon].isSelected)
+            {
+                return;
+            }
+
+            polygons[_indexTouchedPolygon].points.forEach(function (point)
+            {
+                point.x += x - startX;
+                point.y += y - startY;
+            });
+
+            startX = x;
+            startY = y;
+            invalidateAll();
+        });
+
+        $(currentElement).mouseup(function (event)
+        {
+            event.preventDefault();
+            var x = event.offsetX;
+            var y = event.offsetY;
+
+            if (_indexTouchedPolygon == -1)
+            {
+                return;
+            }
+
+            if (_indexTouchedBone > -1 && polygons[_indexTouchedPolygon].points[_indexTouchedBone].isSelected)
+            {
+                polygons[_indexTouchedPolygon].points[_indexTouchedBone].x = x;
+                polygons[_indexTouchedPolygon].points[_indexTouchedBone].y = y;
+                polygons[_indexTouchedPolygon].points[_indexTouchedBone].isSelected = false;
+
+                // if first bone need move first and last point
+                if (_indexTouchedBone == 0)
+                {
+                    polygons[_indexTouchedPolygon].points[polygons[_indexTouchedPolygon].points.length - 1].x = x;
+                    polygons[_indexTouchedPolygon].points[polygons[_indexTouchedPolygon].points.length - 1].y = y;
+                    polygons[_indexTouchedPolygon].points[polygons[_indexTouchedPolygon].points.length - 1].isSelected = false;
+                }
+
+                _indexTouchedPolygon = -1;
+                _indexTouchedBone = -1;
+                invalidateAll();
+                return;
+            }
+
+            if (!polygons[_indexTouchedPolygon].isSelected)
+            {
+                return;
+            }
+
+            polygons[_indexTouchedPolygon].points.forEach(function (point)
+            {
+                point.x += x - startX;
+                point.y += y - startY;
+                point.isSelected = false;
+            });
+            polygons[_indexTouchedPolygon].isSelected = false;
+
+            startX = 0;
+            startY = 0;
+            _indexTouchedPolygon = -1;
+            _indexTouchedBone = -1;
+            invalidateAll();
+        });
+    }
+
+    init();
+    return {
+        loadFromJson: function (stringJson)
+        {
+            polygons = JSON.parse(stringJson);
+            invalidateAll();
+        },
+        getJson: function ()
+        {
+            return JSON.stringify(polygons);
+        }
+    };
+};
